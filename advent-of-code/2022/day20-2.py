a92af83b2ba3d543d7271a2e0582934d0debc84a
Joydeep Mukherjee <joydeepm02@gmail.com>
2022-12-20 12:17:03 -0500

    Add Advent of Code 2022 Day 20

---
+++ advent-of-code/2022/day20-2.py
@@ -0,0 +1,31 @@
+KEY = 811589153
+
+def solve(xs):
+    n = len(xs)
+
+    order = xs[:]
+    xs = [(x, i) for i, x in enumerate(xs)]
+
+    for _ in range(10):
+        for i, x in enumerate(order):
+            if x == 0:
+                continue
+
+            idx = xs.index((x, i))
+            di = x // abs(x)
+
+            src = idx
+            dst = (idx + abs(x) * di) % (n - 1)
+
+            xs.insert(dst, xs.pop(src))
+
+    zero_idx = next(i for i, (x, _) in enumerate(xs) if x == 0)
+    return sum(xs[(zero_idx + offset) % n][0] for offset in [1000, 2000, 3000])
+
+def main():
+    with open("input/20.data") as f:
+        xs = [int(line.strip()) * KEY for line in  f.readlines()]
+        print(solve(xs))
+
+if __name__ == "__main__":
+    main()
